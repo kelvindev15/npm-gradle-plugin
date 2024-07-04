2134032cb708b2920f87eef9fd721bdf6b99253f
Kelvin Olaiya <73821880+kelvin-olaiya@users.noreply.github.com>
2024-07-04 10:25:34 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.gradle.enterprise") version "3.17.5"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.7"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks(true)
+}
+
+rootProject.name = "template-for-gradle-plugins"

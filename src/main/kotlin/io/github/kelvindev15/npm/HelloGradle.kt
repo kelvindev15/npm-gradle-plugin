03bbe02cee16728098e20dd15682579f1f693df7
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-07-04 14:16:58 +0200

    chore: fix detekt issues

--- src/main/kotlin/io/github/kelvindev15/npm/HelloGradle.kt
+++ src/main/kotlin/io/github/kelvindev15/npm/HelloGradle.kt
@@ -4,13 +4,12 @@ import com.google.gson.Gson
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.property
 import org.gradle.kotlin.dsl.register
 import java.io.Serializable
@@ -20,59 +19,99 @@ import java.io.Serializable
  */
 open class HelloGradle : Plugin<Project> {
     override fun apply(target: Project) {
-        val extension = target.extensions.create<NodeExtension>("node")
+        // val extension = target.extensions.create<NodeExtension>("node")
         target.tasks.register<GeneratePackageFileTask>("hello") {
             // name.set(extension.author)
         }
     }
 }
 
+/**
+ * Just a template.
+ */
 open class GeneratePackageFileTask : DefaultTask() {
 
-    @Internal
     private val gson: Gson = Gson()
 
+    /**
+     * the name of the package.
+     */
     @Input
     val name: Property<String> = project.objects.property<String>()
 
+    /**
+     * the version of the package.
+     */
     @Input
     val version: Property<String> = project.objects.property<String>().also {
         it.convention("1.0.0")
     }
 
+    /**
+     * the author of the package.
+     */
     @Input
     val author: Property<String> = project.objects.property<String>().also {
         it.convention("Kelvin Olaiya")
     }
 
+    /**
+     * the description of the package.
+     */
     @Input
     val description: Property<String> = project.objects.property()
 
+    /**
+     * the main file of the package.
+     */
     @Input
     val main: Property<String> = project.objects.property()
 
+    /**
+     * the license of the package.
+     */
     @Input
     val license: Property<String> = project.objects.property()
 
+    /**
+     * the scripts of the package.
+     */
     @Input
     val scripts: Property<Map<String, String>> = project.objects.property()
 
+    /**
+     * the dependencies of the package.
+     */
     @Input
     val dependencies: Property<Map<String, String>> = project.objects.property()
 
+    /**
+     * the devDependencies of the package.
+     */
     @Input
     val devDependencies: Property<Map<String, String>> = project.objects.property()
 
+    /**
+     * the repository of the package.
+     */
     @Input
     val repository: Property<Pair<String, String>> = project.objects.property()
 
+    /**
+     * the homepage of the package.
+     */
     @Input
     val homepage: Property<String> = project.objects.property<String>().also {
         it.convention("kelvin-olaiya.github.io")
     }
 
+    /**
+     * the output file.
+     */
     @OutputFile
-    val output = project.layout.projectDirectory.file("package.json")
+    val output: RegularFileProperty = project.objects.fileProperty().also {
+        it.convention(project.layout.projectDirectory.file("package.json"))
+    }
 
     /**
      * Just a template.
@@ -96,8 +135,14 @@ open class GeneratePackageFileTask : DefaultTask() {
     }
 }
 
+/**
+ * ff.
+ */
 open class NodeExtension(objects: ObjectFactory) : Serializable {
 
+    /**
+     * ff.
+     */
     val npmExecutable: Property<String> = objects.property<String>().also {
         it.convention("npm")
     }
--- src/main/kotlin/io/github/kelvindev15/npm/NpmPackageFile.kt
+++ src/main/kotlin/io/github/kelvindev15/npm/NpmPackageFile.kt
@@ -1,48 +1,90 @@
 package io.github.kelvindev15.npm
 
-enum class DependencyVersion
-
+/**
+ * Represents a dependency in a package.json file.
+ * [name] is the name of the dependency.
+ * [version] is the version of the dependency.
+ */
 data class NpmDependency(
     val name: String,
     val version: String,
 ) {
     companion object {
+        /**
+         * Converts a pair to a [NpmDependency].
+         */
         fun fromMap(map: Map.Entry<String, String>): NpmDependency {
             return NpmDependency(map.key, map.value)
         }
 
+        /**
+         * Converts a map to a set of [NpmDependency].
+         */
         fun fromMap(map: Map<String, String>): Set<NpmDependency> {
             return map.entries.map { fromMap(it) }.toSet()
         }
     }
 }
 
+/**
+ * Represents a repository in a package.json file.
+ * [type] is the type of the repository.
+ * [url] is the URL of the repository.
+ */
 data class NpmRepository(
     val type: String,
     val url: String,
 ) {
     companion object {
+        /**
+         * Converts a pair to a [NpmRepository].
+         */
         fun from(p: Pair<String, String>): NpmRepository {
             return NpmRepository(p.first, p.second)
         }
     }
 }
 
+/**
+ * Represents a script in a package.json file.
+ * [name] is the name of the script.
+ * [command] is the command of the script.
+ */
 data class NpmScript(
     val name: String,
     val command: String,
 ) {
     companion object {
+        /**
+         * Converts a pair to a [NpmScript].
+         */
         fun from(p: Pair<String, String>): NpmScript {
             return NpmScript(p.first, p.second)
         }
 
+        /**
+         * Converts a map to a set of [NpmScript].
+         */
         fun fromMap(map: Map<String, String>): Set<NpmScript> {
             return map.entries.map { from(it.toPair()) }.toSet()
         }
     }
 }
 
+/**
+ * Represents a package.json file.
+ * [name] is the name of the package.
+ * [version] is the version of the package.
+ * [author] is the author of the package.
+ * [description] is the description of the package.
+ * [main] is the main file of the package.
+ * [license] is the license of the package.
+ * [scripts] are the scripts of the package.
+ * [dependencies] are the dependencies of the package.
+ * [devDependencies] are the devDependencies of the package.
+ * [repository] is the repository of the package.
+ * [homepage] is the homepage of the package.
+ */
 data class NpmPackageFile(
     val name: String,
     val version: String = "1.0.0",
@@ -56,6 +98,9 @@ data class NpmPackageFile(
     val repository: NpmRepository,
     val homepage: String,
 ) {
+    /**
+     * Converts the package file to a map.
+     */
     fun toMap(): Map<String, Any> {
         return mapOf(
             "name" to name,
